131327443a8574ba8738468b26cb327155a5da5d
Sofía <sofianombela@gmail.com>
2019-03-19 20:10:13 +0100

    Completed Reversewords

---
+++ 7kyu/Reversewords_v1.js
@@ -0,0 +1,11 @@
+function reverseWords(str) {
+  return str
+    .split(' ')
+    .map(item => {
+      return item
+        .split('')
+        .reverse()
+        .join('');
+    })
+    .join(' ');
+}
